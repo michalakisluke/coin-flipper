103955486be80c6ea8d6de444d95c3f61fccc821
Luke Michalakis <michalakisluke@gmail.com>
2022-01-13 20:15:07 -0500

    <Fix auth and server files>

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,29 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Decision, Rating } = require('../models');
-const { signToken } = require('../utils/auth');
+// const { AuthenticationError } = require('apollo-server-express');
+// const { User, Decision } = require('../models');
+// const { signToken } = require('../utils/auth');
+
+const { user,
+        userId,
+        createUser,
+        login } = require('../controllers/user-controller');
+const { decisions,
+        decisionId,
+        createDecision,
+        deleteDecision,
+        addRating } = require('../controllers/decision-controller');
 
 const resolvers = {
     Query: {
-        
+        user: user,
+        users: userId,
+        decision: decisionId
     },
     Mutation: {
-
+        addUser: createUser,
+        addDecision: createDecision,
+        updateDecision: addRating,
+        deleteDecision: deleteDecision,
+        login: login
     }
 }
 
