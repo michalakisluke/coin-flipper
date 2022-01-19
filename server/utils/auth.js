aa0eed93b3d76c8be874a4639c30724759daf539
Sarah Dickinson <sarahadickinson@gmail.com>
2022-01-19 01:09:50 -0500

    login component created for login/register

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,10 +4,10 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function({ username, email, _id}) {
+    signToken: function({ username, email, _id }) {
         const payload = { username, email, _id };
 
-        return jwt.sign({ data: payload }, secret, {expiresIn: expiration});
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     },
 
     authMiddleware: function({ req }) {
