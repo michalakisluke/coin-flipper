9b47ff6cef0ead18f5b7fd26032daca9dc9ae90e
Luke Michalakis <michalakisluke@gmail.com>
2022-01-18 19:43:51 -0500

    <Build mutations.js>

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,50 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-    mutation addUser()
+    mutation addUser(username: String!, email: String!, password: String!) {
+        addUser(username: $string, email: $string, password: $string) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
 `;
 
 export const ADD_DEC = gql`
-    mutation
+    mutation addDecision(choiceA: String!, choiceB: String!, finalDecision:String!, username: String!, rating: Boolean, description: String) {
+        addDecision(choiceA: $string, choiceB: $string, finalDecision: $string, username: $string, rating: $boolean, description: $string) {
+            _id
+            choiceA
+            choiceB
+            finalDecision
+            username
+            rating
+            description
+        }
+    }
 `;
 
 export const UPDATE_DEC = gql`
+    mutation updateDecision(_id: ID!, choiceA: String, choiceB: String, finalDecision: String, username: String, rating: Boolean, description: String) {
+        updateDecision(_id: $ID, choiceA: $string, choiceB: $string, finalDecision: $string, username: $string, rating: $boolean, description: $string) {
+            choiceA
+            choiceB
+            finalDecision
+            rating
+            description
+        }
+    }
 `;
 
 export const DELETE_DEC = gql`
+    mutation deleteDecision(_id: ID!, username: String!) {
+        deleteDecision(_id: $ID, username: $string) {
+            finalDecision
+        }
+    }
 `;
 
 export const LOGIN_USER = gql`
