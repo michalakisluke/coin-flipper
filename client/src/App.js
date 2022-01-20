fbb815b6dfd7a477cc7f561ee21a98f2fc1ee35b
Luke Michalakis <michalakisluke@gmail.com>
2022-01-19 20:36:20 -0500

    <Rest of the owl>

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,89 @@
 import React, { Component } from "react";
 import './App.css';
-import ShadowCardTitle from './components/ShadowCardTitle'
-import ShadowCardMenu from "../ShadowCardMenu";
-import ShadowCardAbout from "../ShadowCardAbout";
-import ShadowCardAboutCTA from "../ShadowCardAboutCTA";
-import ShadowCardDecision from "../ShadowCardDecision";
-import ShadowCardDisplay from "../ShadowCardDisplay";
-import ShadowCardReview from "../ShadowCardReview";
-import ShadowCardAccount from "../ShadowCardAccount"
+import { Login, Register } from "./components/Login/index";
+import { Nav } from "./components/Nav/index"
+import { Description, Menu } from "./components/Main/index";
+import { Input, Decision, Display } from "./components/Decison/index";
 
-function App(props) {
-  const {
-    shadowCardTitleProps,
-    icontrueLinktrueLightDarkfalseProps,
-    shadowCardAbout2Props,
-    shadowCardAboutCTAProps,
-    shadowCardDisplayProps,
-    shadowCardReviewProps,
-    shadowCardAccount3Props,
-    footbarMobileProps,
-  } = props;
-
+function App() {
   return (
-    <div className="container-center-horizontal">
-      <div className="home screen">
-        <NavbarMobile />
-        <ShadowCardTitle header={shadowCardTitleProps.header} textBody={shadowCardTitleProps.textBody} />
-        <ShadowCardMenu />
-        <IcontrueLinktrueLightDarkfalse
-          howLongDoPayoutsTake={icontrueLinktrueLightDarkfalseProps.howLongDoPayoutsTake}
-          onceYoureSetUp={icontrueLinktrueLightDarkfalseProps.onceYoureSetUp}
-        />
-        <ShadowCardAbout2
-          iconQuestion={shadowCardAbout2Props.iconQuestion}
-          text1={shadowCardAbout2Props.text1}
-          text2={shadowCardAbout2Props.text2}
-        />
-        <ShadowCardAboutCTA
-          iconQuestion={shadowCardAboutCTAProps.iconQuestion}
-          title={shadowCardAboutCTAProps.title}
-          textBody={shadowCardAboutCTAProps.textBody}
-        />
-        <ShadowCardDecision />
-        <ShadowCardDisplay decisionProps={shadowCardDisplayProps.decisionProps} />
-        <ShadowCardReview decisionProps={shadowCardReviewProps.decisionProps} />
-        <ShadowCardAccount3 title={shadowCardAccount3Props.title} />
-        <FootbarMobile src={footbarMobileProps.src} />
+    <div>
+    <Nav></Nav>
+    <main>
+        <Description></Description>
+        <Menu></Menu>
+    </main>
+</div>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    };
+  }
+
+  componentDidMount() {
+    //Add .right by default
+    this.rightSide.classList.add("right");
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Register containerRef={ref => (this.current = ref)} />
+            )}
+          </div>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
       </div>
     </div>
   );
-}
+};
 
-export default Home;
+
+
+export default App;
